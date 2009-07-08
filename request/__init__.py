0f2e312ef8cbda5743880e5663ef31244015f677
Kyle Fuller <inbox@kylefuller.co.uk>
2009-07-08 19:24:16 +0100

    Created base project, and added a license

---
+++ request/__init__.py
@@ -0,0 +1,5 @@
+__version__ = 0.1
+__copyright__ = 'Copyright (c) 2009, Kyle Fuller'
+__licence__ = 'BSD'
+__author__ = 'Kyle Fuller <inbox@kylefuller.co.uk>'
+__URL__ = 'http://kylefuller.co.uk/project/django-request/'
