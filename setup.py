7e4f88ee179c257c43ffdfcf126b778dc81d1cf5
Kyle Fuller <inbox@kylefuller.co.uk>
2009-07-08 22:29:50 +0100

    Created a setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import request
+
+setup(
+    name='django-request',
+    version='%s' % request.__version__,
+    description='django-request is a statistics module for django. It stores requests in a database for admins to see, it can also be used to get statistics on who is online etc.',
+    author='Kyle Fuller',
+    author_email='inbox@kylefuller.co.uk',
+    url='http://kylefuller.co.uk/projects/django-request/',
+    download_url='http://github.com/kylef/django-request/zipball/master',
+    packages=['request'],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
