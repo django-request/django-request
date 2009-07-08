6709a0eab860e761a1fddac13cf95fa6ef09860c
Kyle Fuller <inbox@kylefuller.co.uk>
2009-07-08 22:37:29 +0100

    Created the setting REQUEST_IGNORE_AJAX

--- request/middleware.py
+++ request/middleware.py
@@ -1,7 +1,11 @@
+from django.conf import settings
 from request.models import Request
 
 class RequestMiddleware(object):
     def process_response(self, request, response):
+        if request.is_ajax() and getattr(settings, 'REQUEST_IGNORE_AJAX', False):
+            return response
+        
         r = Request()
         r.from_http_request(request, response)
         
