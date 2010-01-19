331e522b566fdcb473929d1a628f76a5ec37cb9c
Kyle Fuller <inbox@kylefuller.co.uk>
2010-01-19 15:28:53 +0000

    Trying to fix pip install, and set version 0.21

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-## django-request
+django-request
+==============
 
 django-request is a statistics module for django. It stores requests in a database for admins to see, it can also be used to get statistics on who is online etc.
 
@@ -8,6 +9,7 @@ As well as a site statistics module, with the active_users template tag and mana
 
     Request.objects.active_users(minutes=15)
 
-### Detailed documentation
+Detailed documentation
+----------------------
 
 For a detailed documentation of django-request, or how to install django-request please see: [django-request](http://kylefuller.co.uk/projects/django-request/) or the docs/ directory.
--- setup.py
+++ setup.py
@@ -6,13 +6,20 @@ setup(
     name='django-request',
     version='%s' % request.__version__,
     description='django-request is a statistics module for django. It stores requests in a database for admins to see, it can also be used to get statistics on who is online etc.',
+    long_description="""
+    django-request is a statistics module for django. It stores requests in a database for admins to see, it can also be used to get statistics on who is online etc.
+    
+    .. image:: http://media.kylefuller.co.uk/projects/django-request/graph.png
+    
+    As well as a site statistics module, with the active_users template tag and manager method you can also use django-request to show who is online in a certain time.
+    """,
     author='Kyle Fuller',
     author_email='inbox@kylefuller.co.uk',
-    url='http://kylefuller.co.uk/projects/django-request/',
+    url=request.__URL__,
     download_url='http://github.com/kylef/django-request/zipball/%s' % request.__version__,
-    packages=['request', 'request.templatetags'],
+    packages=['request', 'request.templatetags', 'request.management', 'request.management.commands'],
     package_data={'request': ['templates/admin/request/*.html', 'templates/admin/request/request/*.html']},
-    license='BSD',
+    license=request.__licence__,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
