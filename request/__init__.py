e985163d189883a2419e34021971709c9c7498c0
Kyle Fuller <inbox@kylefuller.co.uk>
2010-06-28 11:13:28 +0100

    Add jezdez to the authors

--- request/__init__.py
+++ request/__init__.py
@@ -1,5 +1,5 @@
 __version__ = 0.23
 __copyright__ = 'Copyright (c) 2009, Kyle Fuller'
 __licence__ = 'BSD'
-__author__ = 'Kyle Fuller <inbox@kylefuller.co.uk>, krisje8 <krisje8@gmail.com>'
+__author__ = 'Kyle Fuller <inbox@kylefuller.co.uk>, Jannis Leidel (jezdez), krisje8 <krisje8@gmail.com>'
 __URL__ = 'http://kylefuller.co.uk/project/django-request/'
