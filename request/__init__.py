2f8f0b6f7a8ee32396fc5cfd76b873f0daeedf52
Kyle Fuller <inbox@kylefuller.co.uk>
2010-06-28 09:29:39 +0100

    Set version to 0.23

--- request/__init__.py
+++ request/__init__.py
@@ -1,4 +1,4 @@
-__version__ = 0.21
+__version__ = 0.23
 __copyright__ = 'Copyright (c) 2009, Kyle Fuller'
 __licence__ = 'BSD'
 __author__ = 'Kyle Fuller <inbox@kylefuller.co.uk>, krisje8 <krisje8@gmail.com>'
