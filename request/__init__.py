5f8886f5da3466e6b9de3276e77171bcef71bd6f
Kyle Fuller <inbox@kylefuller.co.uk>
2010-07-04 01:29:26 +0100

    Update the version number to 0.30

--- request/__init__.py
+++ request/__init__.py
@@ -1,4 +1,4 @@
-__version__ = 0.23
+__version__ = 0.30
 __copyright__ = 'Copyright (c) 2009, Kyle Fuller'
 __licence__ = 'BSD'
 __author__ = 'Kyle Fuller <inbox@kylefuller.co.uk>, Jannis Leidel (jezdez), krisje8 <krisje8@gmail.com>'
