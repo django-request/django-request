4f3f62c76362626206f44175cd6563bc6711505c
Kyle Fuller <inbox@kylefuller.co.uk>
2013-10-04 23:56:11 +0100

    Extend copyright to incluld 2013

--- request/__init__.py
+++ request/__init__.py
@@ -1,5 +1,5 @@
 __version__ = 0.30
-__copyright__ = 'Copyright (c) 2009, Kyle Fuller'
+__copyright__ = 'Copyright (c) 2009-2013, Kyle Fuller'
 __licence__ = 'BSD'
 __author__ = [
     'Kyle Fuller <inbox@kylefuller.co.uk>',
@@ -7,3 +7,4 @@ __author__ = [
     'krisje8 <krisje8@gmail.com>',
 ]
 __URL__ = 'http://kylefuller.co.uk/project/django-request/'
+
