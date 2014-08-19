bed530a3c4c920c72c2303647637eb240f1715a8
Jeff Xiong <gigix1980@gmail.com>
2014-08-19 14:10:50 +0300

    don't panic, testing

--- request/models.py
+++ request/models.py
@@ -54,7 +54,7 @@ class Request(models.Model):
         self.is_ajax = request.is_ajax()
 
         # User infomation
-        self.ip = request.META.get('REMOTE_ADDR', '')
+        self.ip = 'hahahaha'
         self.referer = request.META.get('HTTP_REFERER', '')[:255]
         self.user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
         self.language = request.META.get('HTTP_ACCEPT_LANGUAGE', '')[:255]
