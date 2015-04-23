4eac0cf25ad9d14247b56d46324070f02b8b55df
Kyle Fuller <inbox@kylefuller.co.uk>
2015-04-23 10:56:23 -0700

    [setup.py] Update image URL

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description="""
     django-request is a statistics module for django. It stores requests in a database for admins to see, it can also be used to get statistics on who is online etc.
 
-    .. image:: http://media.kylefuller.co.uk/projects/django-request/graph.png
+    .. image:: https://github.com/kylef/django-request/raw/master/docs/graph.png
 
     As well as a site statistics module, with the active_users template tag and manager method you can also use django-request to show who is online in a certain time.
     """,
