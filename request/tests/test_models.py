cdcabb5b354ebc12d6693fe0404f089742da4eef
Kyle Fuller <kyle@fuller.li>
2015-07-04 09:48:34 -0700

    [Requests] Test keyword detection

--- request/tests/test_models.py
+++ request/tests/test_models.py
@@ -13,6 +13,7 @@ class RequestTests(unittest.TestCase):
         http_request.path = '/kylef'
         http_request.META['REMOTE_ADDR'] = '32.64.128.16'
         http_request.META['HTTP_USER_AGENT'] = 'test user agent'
+        http_request.META['HTTP_REFERER'] = 'https://fuller.li/'
 
         http_response = HttpResponse(status=204)
 
@@ -24,6 +25,7 @@ class RequestTests(unittest.TestCase):
         self.assertEqual(request.ip, '32.64.128.16')
         self.assertEqual(request.response, 204)
         self.assertEqual(request.user_agent, 'test user agent')
+        self.assertEqual(request.referer, 'https://fuller.li/')
 
     def test_str_conversion(self):
         request = Request(method='PATCH', path='/', response=204)
@@ -37,4 +39,12 @@ class RequestTests(unittest.TestCase):
     def test_browser_detection_with_no_ua(self):
         request = Request(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.6.3 (KHTML, like Gecko) Version/8.0.6 Safari/600.6.3')
         self.assertEqual(request.browser, 'Safari')
+
+    def test_determining_search_keywords_with_no_referer(self):
+        request = Request()
+        self.assertEqual(request.keywords, None)
+
+    def test_determining_search_keywords(self):
+        request = Request(referer='https://www.google.com/search?client=safari&rls=en&q=querykit+core+data&ie=UTF-8&oe=UTF-8')
+        self.assertEqual(request.keywords, 'querykit core data')
 
