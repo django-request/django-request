915343c933810889a14cd1f85989e4ccb8f0e459
Kyle Fuller <kyle@fuller.li>
2015-07-04 09:42:04 -0700

    [Request] Test browser detection

--- request/tests/test_models.py
+++ request/tests/test_models.py
@@ -12,6 +12,7 @@ class RequestTests(unittest.TestCase):
         http_request.method = 'PATCH'
         http_request.path = '/kylef'
         http_request.META['REMOTE_ADDR'] = '32.64.128.16'
+        http_request.META['HTTP_USER_AGENT'] = 'test user agent'
 
         http_response = HttpResponse(status=204)
 
@@ -22,8 +23,18 @@ class RequestTests(unittest.TestCase):
         self.assertEqual(request.method, 'PATCH')
         self.assertEqual(request.ip, '32.64.128.16')
         self.assertEqual(request.response, 204)
+        self.assertEqual(request.user_agent, 'test user agent')
 
     def test_str_conversion(self):
         request = Request(method='PATCH', path='/', response=204)
         request.time = datetime.now()
         self.assertEqual(str(request), '[{}] PATCH / 204'.format(request.time))
+
+    def test_browser_detection_with_no_ua(self):
+        request = Request(method='GET', path='/', response=200)
+        self.assertEqual(request.browser, None)
+
+    def test_browser_detection_with_no_ua(self):
+        request = Request(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.6.3 (KHTML, like Gecko) Version/8.0.6 Safari/600.6.3')
+        self.assertEqual(request.browser, 'Safari')
+
