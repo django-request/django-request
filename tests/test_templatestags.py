ff9dc182b25d056e983072046f060492e5b33b34
ZuluPro <montheanthony@hotmail.com>
2016-03-30 14:55:12 -0400

    Added tests for request.templatetags.request_tag

--- tests/test_templatestags.py
+++ tests/test_templatestags.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+from django import template
 from request.templatetags.request_admin import trunc, pie_chart
+from request.templatetags.request_tag import ActiveUserNode, active_users
 
 
 class RequestAdminTruncTest(TestCase):
@@ -18,4 +20,51 @@ class RequestAdminTruncTest(TestCase):
 class RequestAdminPieChart(TestCase):
     def test_pie_chart(self):
         inventory = ['apple', 'lemon', 'apple', 'orange', 'lemon', 'lemon']
-        pie_chart(inventory)
+        result = pie_chart(inventory)
+        self.assertTrue(result.startswith('http://chart.apis.google.com/chart?'))
+        self.assertIn('chs=440x190', result)
+
+        result = pie_chart(inventory, width=100, height=100)
+        self.assertTrue(result.startswith('http://chart.apis.google.com/chart?'))
+        self.assertIn('chs=100x100', result)
+
+
+# TODO: It's unused but add a parser: template.debug.DebugParser
+class RequestTagActiveUserNodeTest(TestCase):
+    def test_syntax_error_bad_args_number(self):
+        token = template.base.Token(2, 'active_users foo')
+        self.assertRaises(template.TemplateSyntaxError, ActiveUserNode, None, token)
+
+    def test_in_amount_duration_as_varname(self):
+        token = template.base.Token(2, "active_users in 10 minutes as users")
+        node = ActiveUserNode(None, token)
+        self.assertEqual(node.as_varname, 'users')
+
+    def test_in_amount_duration_as_varname_error_bad_number(self):
+        token = template.base.Token(2, "active_users in foo minutes as user_list")
+        self.assertRaises(template.TemplateSyntaxError, ActiveUserNode, None, token)
+
+    def test_as_varname(self):
+        token = template.base.Token(2, "active_users as user_list")
+        node = ActiveUserNode(None, token)
+        self.assertEqual(node.as_varname, 'user_list')
+
+        token = template.base.Token(2, "active_users as users")
+        node = ActiveUserNode(None, token)
+        self.assertEqual(node.as_varname, 'users')
+
+    def test_no_args(self):
+        token = template.base.Token(2, "active_users")
+        ActiveUserNode(None, token)
+
+    def test_render(self):
+        token = template.base.Token(2, "active_users")
+        node = ActiveUserNode(None, token)
+        self.assertEqual('', node.render({}))
+
+
+class RequestTagActiveUsersTest(TestCase):
+    def test_active_users(self):
+        token = template.base.Token(2, "active_users")
+        node = active_users(None, token)
+        self.assertIsInstance(node, ActiveUserNode)
