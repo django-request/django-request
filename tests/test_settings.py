3b502dc036815a442b4266662204f45c5968f5c2
ZuluPro <montheanthony@hotmail.com>
2016-03-31 00:52:04 -0400

    Added tests for request.admin

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,3 +1,4 @@
+SECRET_KEY = '&qaeg(m5s0rpdj-wx@hrc3vpu)v@@n$if67ba-4e9&kk+j$$c+'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -17,6 +18,20 @@ PASSWORD_HASHERS = [
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.admin',
     'tests',
     'request',
 ]
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+STATIC_URL = '/static/'
+
+ROOT_URLCONF = '.'.join([m for m in __name__.split('.')][:-1] + ['test_urls'])
