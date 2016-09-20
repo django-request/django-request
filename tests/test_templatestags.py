c450fa7f63d68238f15dd6c70ee13e93b78d6125
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2016-09-20 22:04:14 +0200

    Removed table summary attribute that is not supported in HTML5. Changed trunc to Django truncatechars template tag.

--- tests/test_templatestags.py
+++ tests/test_templatestags.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import template
 from django.test import TestCase
-from request.templatetags.request_admin import pie_chart, trunc
+from request.templatetags.request_admin import pie_chart
 from request.templatetags.request_tag import ActiveUserNode, active_users
-
-
-class RequestAdminTruncTest(TestCase):
-    def test_not_truncate(self):
-        string = "A" * 10
-        new_string = trunc(string, 20)
-        self.assertEqual(len(new_string), 10)
-
-    def test_truncate(self):
-        string = "A" * 10
-        new_string = trunc(string, 5)
-        self.assertLess(len(new_string), 6)
 
 
 class RequestAdminPieChart(TestCase):
