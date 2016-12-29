5ede4ffebddd5cd4c26b12395e7b7cfd7f8bb032
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2016-12-29 06:42:13 +0100

    Added python versions to setup.

--- setup.py
+++ setup.py
@@ -38,5 +38,9 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-    ]
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
