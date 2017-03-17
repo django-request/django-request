f0de9b853690eea17f5c9b67b9171db0f7e98872
Steve Lacey <steve@stevelacey.net>
2017-03-17 16:19:04 +0800

    Log IP from X_FORWARDED_FOR if passed
    Many proxies will pass this and the REMOTE_ADDR will be something internal e.g. Heroku

--- request/models.py
+++ request/models.py
@@ -64,7 +64,7 @@ class Request(models.Model):
         self.is_ajax = request.is_ajax()
 
         # User infomation
-        self.ip = request.META.get('REMOTE_ADDR', '')
+        self.ip = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR', '')).split(',')[0]
         self.referer = request.META.get('HTTP_REFERER', '')[:255]
         self.user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
         self.language = request.META.get('HTTP_ACCEPT_LANGUAGE', '')[:255]
