7f6b1e4f3d3438153b9aa59c1572844b05afa277
Steve Lacey <steve@stevelacey.net>
2017-05-04 04:01:16 +0900

    Fixed #138 -- Set on_delete=SET_NULL on request.user.

---
+++ request/migrations/0005_alter_request_user.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('request', '0004_alter_time_timezone_default'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='request',
+            name='user',
+            field=models.ForeignKey(blank=True, null=True, on_delete=models.SET_NULL, to=settings.AUTH_USER_MODEL, verbose_name='user'),
+        ),
+    ]
