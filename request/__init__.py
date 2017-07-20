3b0b37eda7c7c8418d90cfed2b0700ece049b978
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-07-20 21:29:13 +0200

    Release 1.5.3.

--- request/__init__.py
+++ request/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.5.2'
+__version__ = '1.5.3'
 __copyright__ = 'Copyright (c) 2009-2017, Kyle Fuller, Mariusz Felisiak'
 __licence__ = 'BSD'
 __author__ = [
@@ -7,4 +7,4 @@ __author__ = [
     'krisje8 <krisje8@gmail.com>',
     'Mariusz Felisiak <felisiak.mariusz@gmail.com>',
 ]
-__URL__ = 'http://django-request.readthedocs.org/en/latest/'
+__URL__ = 'https://django-request.readthedocs.org/en/latest/'
