a17d5b2defac5f5a28fe229bfda588ed62e85e73
Tobias Megies <megies@geophysik.uni-muenchen.de>
2017-09-25 10:32:16 +0200

    put traffic diagram legend to top-left corner
    otherwise obscures the most recent data, which is likely the most
    interesting

--- request/static/request/js/jquery.flot.js
+++ request/static/request/js/jquery.flot.js
@@ -45,7 +45,7 @@
                     labelFormatter: null, // fn: string -> string
                     labelBoxBorderColor: "#ccc", // border color for the little label boxes
                     container: null, // container (as jQuery object) to put legend in, null means default on top of graph
-                    position: "ne", // position of default legend container within plot
+                    position: "nw", // position of default legend container within plot
                     margin: 5, // distance from grid edge to default legend container within plot
                     backgroundColor: null, // null means auto-detect
                     backgroundOpacity: 0.85 // set to 0 to avoid background
