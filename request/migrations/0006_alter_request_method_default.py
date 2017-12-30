df12934845523c142de0dad5f61bb3338c49d866
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-12-30 20:56:12 +0100

    Fixed #150 -- Added migration to change Request.method default value.

---
+++ request/migrations/0006_alter_request_method_default.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('request', '0005_alter_request_user'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='request',
+            name='method',
+            field=models.CharField(default='GET', max_length=7, verbose_name='method'),
+        ),
+    ]
