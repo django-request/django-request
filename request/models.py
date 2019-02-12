8a27619610118ffbd827e4186915d9e0f5ee1659
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-02-12 19:52:42 +0100

    Used six package instead of django.utils.six.
    django.utils.six has been removed in Django 3.0.

--- request/models.py
+++ request/models.py
@@ -5,8 +5,8 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from six import python_2_unicode_compatible
 
 from . import settings as request_settings
 from .managers import RequestManager
