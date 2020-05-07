django-request/django-request

commit 0adc76d8b8e4bf9fc19e0e4ca005da9b9a59191d

Simplified RequestQuerySet.month()/day() by using builtin Django lookups.

request/managers.py
@@ -36,23 +36,10 @@ class RequestQuerySet(models.query.QuerySet):
                     raise TypeError('Request.objects.month() takes exactly 2 arguments')
             except ValueError:
                 return
-
         # Truncate to date.
         if isinstance(date, datetime.datetime):
             date = date.date()
-        # Calculate first and last day of month, for use in a date-range lookup.
-        first_day = date.replace(day=1)
-        if first_day.month == 12:
-            last_day = first_day.replace(year=first_day.year + 1, month=1)
-        else:
-            last_day = first_day.replace(month=first_day.month + 1)
-
-        lookup_kwargs = {
-            'time__gte': first_day,
-            'time__lt': last_day,
-        }
-
-        return self.filter(**lookup_kwargs)
+        return self.filter(time__year=date.year, time__month=date.month)
 
     def week(self, year, week):
         try:
@@ -79,11 +66,7 @@ class RequestQuerySet(models.query.QuerySet):
                     raise TypeError('Request.objects.day() takes exactly 3 arguments')
             except ValueError:
                 return
-
-        return self.filter(time__range=(
-            datetime.datetime.combine(date, datetime.time.min),
-            datetime.datetime.combine(date, datetime.time.max),
-        ))
+        return self.filter(time__date=date)
 
     def today(self):
         return self.day(date=datetime.date.today())

commit 4deb4f895225434e049dbf84d0583859125ffec2

Refs #200 -- Fixed performance regression in RequestQuerySet.day().
Regression in 0adc76d8b8e4bf9fc19e0e4ca005da9b9a59191d.

request/managers.py
@@ -71,7 +71,10 @@ class RequestQuerySet(models.query.QuerySet):
                     raise TypeError('Request.objects.day() takes exactly 3 arguments')
             except ValueError:
                 return
-        return self.filter(time__date=date)
+        return self.filter(time__range=(
+            datetime.datetime.combine(date, datetime.time.min),
+            datetime.datetime.combine(date, datetime.time.max),
+        ))
 
     def today(self):
         return self.day(date=datetime.date.today())