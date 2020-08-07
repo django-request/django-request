70d2b6b817ef7e372345df66592963048cc943c1
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-08-07 22:42:31 +0200

    Bumped minimum isort version to 5.1.0.

--- request/settings.py
+++ request/settings.py
@@ -35,8 +35,8 @@ PLUGINS = getattr(settings, 'REQUEST_PLUGINS', (
 ))
 
 try:
-    from django.http import HttpRequest
     from django.contrib.sites.shortcuts import get_current_site
+    from django.http import HttpRequest
     BASE_URL = getattr(settings, 'REQUEST_BASE_URL', 'http://{0}'.format(get_current_site(HttpRequest()).domain))
 except Exception:
     BASE_URL = getattr(settings, 'REQUEST_BASE_URL', 'http://127.0.0.1')
