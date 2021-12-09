283b39c66605f2137a7d5cab87b998253ac34d1f
alfonsrv <alfonsrv@users.noreply.github.com>
2021-12-09 16:48:30 +0100

    Made REQUEST_IGNORE_AJAX ignore htmx requests.

--- request/utils.py
+++ request/utils.py
@@ -158,7 +158,10 @@ def get_verbose_name(class_name):
 
 
 def request_is_ajax(request):
-    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+    return (
+        request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest' or
+        request.META.get('HTTP_HX_REQUEST') == 'true'  # htmx
+    )
 
 
 def handle_naive_datetime(value):
