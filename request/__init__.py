f1b3204b68481617cf04b64c9c270dfbb52a7cc2
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-12-20 20:32:42 +0100

    Improved setup.cfg.

--- request/__init__.py
+++ request/__init__.py
@@ -1,10 +1,8 @@
 __version__ = '1.5.6'
 __copyright__ = 'Copyright (c) 2009-2020, Kyle Fuller, Mariusz Felisiak'
-__licence__ = 'BSD'
 __author__ = [
     'Kyle Fuller <kyle@fuller.li>',
     'Jannis Leidel (jezdez)',
     'krisje8 <krisje8@gmail.com>',
     'Mariusz Felisiak <felisiak.mariusz@gmail.com>',
 ]
-__URL__ = 'https://django-request.readthedocs.io/en/latest/'
