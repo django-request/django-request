eab2003a0be3a3ba2932d6c5d939ef9f252dc6ff
wahajmurtaza <wahajmurtaza.27@iiee.edu.pk>
2022-09-04 15:23:03 +0500

    Added distinction of Edge / Chrome in requests

--- request/utils.py
+++ request/utils.py
@@ -83,7 +83,8 @@ browsers = Patterns(
         'AOL',
     ),
     (r'Camino/(?P<version>[-.\w]+)', 'Camino'),
-    (r'Chrome/(?P<version>[-.\w]+)', 'Google Chrome'),
+    (r'Chrome/(?P<version>[-.\w]+)(?!.*Edg).*', 'Google Chrome'),
+    (r'Edg(A|iOS)?/(?P<version>[-.\w]+)', 'Microsoft Edge'),
     (r'Firefox(/(?P<version>[-.\w]+)?)', 'Firefox'),
     (
         r'Mozilla/(?P<mozilla_version>[-.\w]+) \(compatible; ( ?)MSIE (?P<version>[-.\w]+); ' +
