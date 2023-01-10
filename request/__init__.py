fca28c8570a39ad875eedafcc9921c8fd2831fe0
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-01-10 17:08:34 +0100

    Release 1.6.2.

--- request/__init__.py
+++ request/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '1.6.1'
-__copyright__ = 'Copyright (c) 2009-2022, Kyle Fuller, Mariusz Felisiak'
+__version__ = '1.6.2'
+__copyright__ = 'Copyright (c) 2009-2023, Kyle Fuller, Mariusz Felisiak'
 __author__ = [
     'Kyle Fuller <kyle@fuller.li>',
     'Jannis Leidel (jezdez)',
